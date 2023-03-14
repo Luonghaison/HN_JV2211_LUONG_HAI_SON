e8a811558388eb855c4a4f69934aed52511bb0d3
unknown <keibin.wru@gmail.com>
2023-03-14 11:29:01 +0700

    3

--- src/Bai1.java
+++ src/Bai1.java
@@ -4,19 +4,23 @@ public class Bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] canChi = {"Giáp Tý", "Ất Sửu", "Bính Dần", "Đinh Mão", "Mậu Thìn", "Kỷ Tỵ",
-                "Canh Ngọ", "Tân Mùi", "Nhâm Thân", "Quý Dậu", "Giáp Tuất", "Ất Hợi"};
+        String[] listCan = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
+        String[] listChi = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi"};
 
         System.out.println("Nhập số tuổi của bạn vào đây:");
         int age = sc.nextInt();
-        System.out.println("Nhập năm hiện tại vào đây:");
-        int nowYear = sc.nextInt();
+        int birthYear = 2023 - age;
+
+        int canIndex = (birthYear-1)%10+1;
+        if(canIndex==10){canIndex=0;}
+        int chiIndex = (birthYear-1)%12+1;
+        if(chiIndex==12){chiIndex=0;}
 
 
-        int nowCan = (nowYear - 4) % 10;
-        int ageCan = age % 10;
-        int index = (nowCan + ageCan) % 12;
-        System.out.println("Can chi của bạn là: "+ canChi[index]);
+        String can = listCan[canIndex];
+        String chi = listChi[chiIndex];
+
+        System.out.println("Bạn năm nay " +age+ " tuổi thì Can chi của bạn là: "+can+" "+chi);
 
 
     }
